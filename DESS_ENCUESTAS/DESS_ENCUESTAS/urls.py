1dde1e8dd196628e0dff2064dd0efd1a19ecbc33
MIGUELAGC29 <agc_miguel@hotmail.com>
2022-12-05 22:03:55 -0600

    MG: se crea una app para el login, ademàs se genera la vista, queda pendiente anexar BootStrap y comenzar el código

--- DESS_ENCUESTAS/DESS_ENCUESTAS/urls.py
+++ DESS_ENCUESTAS/DESS_ENCUESTAS/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from PRUEBA import views  
+from django.urls import path 
+from Login import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('holaMundo/', views.holaMundo, name="hola-mundo"),
+    path('', views.peticionCurp, name="PeticionCurp"),
 ]
