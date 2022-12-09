8df543972e32009b4356a61e3af9d9840cc436d5
MIGUELAGC29 <agc_miguel@hotmail.com>
2022-12-09 16:05:12 -0600

    MG: se agrega el header y el footer en el template de Login

---
+++ DESS_ENCUESTAS/static/temporal/styles/redesSociales.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $('#imgFacebook').attr('href', 'https://www.facebook.com/DEySS/');
+    $('#imgTwitter').attr('href', 'https://twitter.com/IPN_DEySS');
+    $('#imgYoutube').attr('href', 'https://www.youtube.com/user/EgresadosIPN');
+    $('#imgFacebook').attr('target', '_blank');
+    $('#imgTwitter').attr('target', '_blank');
+    $('#imgYoutube').attr('target', '_blank');
+
+});
