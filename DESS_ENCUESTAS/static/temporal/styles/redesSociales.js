206960848015940416845f57da22c0031dc6f00d
MIGUELAGC29 <agc_miguel@hotmail.com>
2022-12-10 13:58:08 -0600

    MG: Se realiza el header del gobierno e Instituto Politécnico Nacional, queda pendeinte el diseño responsivo

--- DESS_ENCUESTAS/static/temporal/styles/redesSociales.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    $('#imgFacebook').attr('href', 'https://www.facebook.com/DEySS/');
-    $('#imgTwitter').attr('href', 'https://twitter.com/IPN_DEySS');
-    $('#imgYoutube').attr('href', 'https://www.youtube.com/user/EgresadosIPN');
-    $('#imgFacebook').attr('target', '_blank');
-    $('#imgTwitter').attr('target', '_blank');
-    $('#imgYoutube').attr('target', '_blank');
-
-});
