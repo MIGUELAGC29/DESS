b8364d743fc92d9639d3ad54d2451df049d44a04
MIGUELAGC29 <agc_miguel@hotmail.com>
2022-12-11 22:05:15 -0600

    MG: se conecta el modal con algun template para continuar el proceso

---
+++ DESS_ENCUESTAS/Login/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from Login.views import *
+
+
+urlpatterns = [
+    path('', peticionCurp, name="PeticionCurp"),
+    path('registro/', registroUsuario, name="Registro_Usuario"),
+    path('error/', error, name="Error"),
+]
--- DESS_ENCUESTAS/Login/views.py
+++ DESS_ENCUESTAS/Login/views.py
@@ -3,3 +3,22 @@ from django.shortcuts import render
 
 def peticionCurp(request):
     return render(request, 'pedirCurp.html')
+
+
+def registroUsuario(request):
+    if request.method == "POST" or request.method == "GET":
+        curp = request.POST['curp']
+        #SE HACE EL DESMADRE DE LA RENAPO
+        nombre = "Miguel"
+        ap_paterno = "González"
+        ap_materno = "Cruz"
+        
+    return render(request, 'registroUsuario.html', {
+        'curp': curp,
+        'nombre': nombre,
+        'ap_paterno': ap_paterno,
+        'ap_materno': ap_materno,    })
+    
+    
+def error(request):
+    return render(request, 'error.html')
