d5118d83187cd4edf4843ab5d8092f51085cbf2b
MIGUELAGC29 <agc_miguel@hotmail.com>
2022-12-12 22:00:03 -0600

    MG: queda funcionando correctamente el frontend, falta la confirmación de los demás datos necesarios

--- DESS_ENCUESTAS/Login/views.py
+++ DESS_ENCUESTAS/Login/views.py
@@ -9,76 +9,47 @@ def peticionCurp(request):
 
 
 def registroUsuario(request):
+    
     url = "https://api.plataforma.ipn.mx/publico/general/secure/renapo/curp/consultar"
     user = "deyssuser"
     password = "p5HHNbdq7JUt5Hnqwv2erWva6hPbKtk8"
+    
     if request.method == "POST" or request.method == "GET":
         curp = request.POST['curp']
         json = "{" + "\"data\":{" + "\"curp\":" + "\"" + curp + "\"" + "} }"
-        request = requests.post(url, auth = HTTPBasicAuth(user,password), data=json)
-        if(request != None):
+        request_renapo = requests.post(url, auth = HTTPBasicAuth(user,password), data=json)
+        if(request_renapo != None):
             try:
-                jsonData = request.json()
-                nombres = jsonData.get('nombres')
-                ap_paterno = jsonData.get('apellido1')
-                ap_materno = jsonData.get('apellido2')
-                return render(request, 'registroUsuario.html', {
-                'curp': curp,
-                'nombres': nombres,
-                'ap_paterno': ap_paterno,
-                'ap_materno': ap_materno,    })
+                jsonData = request_renapo.json()
+                if(jsonData.get('@statusOper') == "NO EXITOSO"):
+                    error = jsonData.get('@message')
+                    return render(request, 'pedirCurp.html', {
+                    'error':error,
+                    'value':curp,
+                    })
+                elif(jsonData.get('@statusOper') == "EXITOSO"):
+                    nombres = jsonData.get('nombres')
+                    ap_paterno = jsonData.get('apellido1')
+                    ap_materno = jsonData.get('apellido2')
+                    return render(request, 'registroUsuario.html', {
+                        'curp': curp,
+                        'nombres': nombres,
+                        'ap_paterno': ap_paterno,
+                        'ap_materno': ap_materno,    
+                        })
             except:
-                print("NO GENIAL")
+                error = "SOLICITUD NO REALIZADA, INTENTELO MÁS TARDE1"
+                return render(request, 'pedirCurp.html', {
+                    'error':error,
+                    'value':curp,
+                    })
         else:
-            messages.add_message(request=request, level=messages.ERROR, message="SOLICITUD NO REALIZADA, INTENTELO MÁS TARDE")
+            error = "SOLICITUD NO REALIZADA, INTENTELO MÁS TARDE2"
             return render(request, 'pedirCurp.html', {
                 'error':error,
                 'value':curp,
-            })
-
-
-
-
-
-
-
-
-
-
-
-import base64
-import requests 
-from requests.auth import HTTPBasicAuth
-
-
-url = "https://api.plataforma.ipn.mx/publico/general/secure/renapo/curp/consultar"
-user = "deyssuser"
-password = "p5HHNbdq7JUt5Hnqwv2erWva6hPbKtk8"
-curp = "MACA870610MDFTSD01"
-
-
-
-json = "{" + "\"data\":{" + "\"curp\":" + "\"" + curp + "\"" + "} }"
-request = requests.post(url, auth = HTTPBasicAuth(user,password), data=json)
-if(request != None):
-    try:
-        jsonData=request.json()
-        print(jsonData.get('nombres'))
-    except:
-        print("NO GENIAL")
-
-    
-
-
-
-
-
-        
+                })
             
             
-
-        
-
-
-    
-  
+            
+#def guardarUsuario(request):
