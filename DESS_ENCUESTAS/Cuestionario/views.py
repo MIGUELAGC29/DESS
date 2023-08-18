70ff4a0ff1c1e4eb66d4e78045fc2361c6bab5ba
MIGUELAGC29 <agc_miguel@hotmail.com>
2023-08-17 21:08:37 -0600

    MG: se agregan todas las preguntas del cuestionario

--- DESS_ENCUESTAS/Cuestionario/views.py
+++ DESS_ENCUESTAS/Cuestionario/views.py
@@ -53,9 +53,15 @@ def finalizado(request):
         
         pregunta_16 = request.POST['pregunta_16']
         
-        pregunta_17 = request.POST['pregunta_17']
+        if(request.POST['pregunta_17'] == "Otro"):
+            pregunta_17 = request.POST['pregunta_17'] + ": " + request.POST['pregunta_17_otro']
+        else:
+            pregunta_17 = request.POST['pregunta_17']
 
-        pregunta_18 = request.POST['pregunta_18']
+        if(request.POST['pregunta_18'] == "Otro"):
+            pregunta_18 = request.POST['pregunta_18'] + ": " + request.POST['pregunta_18_otro']
+        else:
+            pregunta_18 = request.POST['pregunta_18']
 
         pregunta_19 = request.POST['pregunta_19']
 
@@ -65,7 +71,41 @@ def finalizado(request):
         
         pregunta_22 = request.POST['pregunta_22']
             
+        if(request.POST['pregunta_23'] == "Otro"):
+            pregunta_23 = request.POST['pregunta_23'] + ": " + request.POST['pregunta_23_otro']
+        else:
+            pregunta_23 = request.POST['pregunta_23']
 
+        pregunta_24 = request.POST['pregunta_24']
+        
+        pregunta_25 = request.POST['pregunta_25']
+        
+        pregunta_26 = request.POST['pregunta_26']
+
+        pregunta_27 = request.POST['pregunta_27']
+
+        pregunta_28 = request.POST['pregunta_28']
+
+        pregunta_29 = request.POST['pregunta_29']
+
+        pregunta_30 = request.POST['pregunta_30']
+
+        pregunta_31 = request.POST['pregunta_31']
+
+        pregunta_32 = request.POST['pregunta_32']
+
+        pregunta_33 = request.POST['pregunta_33']
+
+        pregunta_34 = request.POST['pregunta_34']
+
+        pregunta_35 = request.POST['pregunta_35']
+
+        pregunta_36 = request.POST['pregunta_36']
+        
+        pregunta_37 = request.POST['pregunta_37']
+        
+        pregunta_38 = request.POST['pregunta_38']
+        
         
         
         print(pregunta_1)
@@ -90,6 +130,25 @@ def finalizado(request):
         print(pregunta_20)
         print(pregunta_21)
         print(pregunta_22)
+        print(pregunta_23)
+        print(pregunta_24)
+        print(pregunta_25)
+        print(pregunta_26)
+        print(pregunta_27)
+        print(pregunta_28)
+        print(pregunta_29)
+        print(pregunta_30)
+        print(pregunta_31)
+        print(pregunta_32)
+        print(pregunta_33)
+        print(pregunta_34)
+        print(pregunta_35)
+        print(pregunta_36)
+        print(pregunta_37)
+        print(pregunta_38)
+
+
+
         
 
         
